d3c8eefa2fb3b1133903d1a302abc9dba1407fcb
smsmeltzer <ssmeltzer@tamu.edu>
2025-02-05 15:09:41 -0600

    Update InteractableObj.cs

--- Assets/Scripts/InteractableObj.cs
+++ Assets/Scripts/InteractableObj.cs
@@ -4,38 +4,60 @@ using UnityEngine;
 
 public class InteractableObj : MonoBehaviour
 {
+
     [SerializeField] private GameObject Popup;
+    public bool PressAndHold = false;
+    private bool interacting = false;
+    private bool keyPressed = false;
 
     private void Start()
     {
         Popup.SetActive(false);
     }
 
+    private void Update()
+    {
+        if (interacting)
+        {
+            if (Input.GetKeyDown(KeyCode.E) && !keyPressed)
+            {
+                keyPressed = true;
+                StartInteraction();
+            }
+            else if (Input.GetKeyUp(KeyCode.E))
+            {
+                keyPressed = false;
+                EndInteraction();
+            }
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") && collision.gameObject.GetComponent<PlayerInteractor>().enabled)
         {
-            StartInteraction();
+            Popup.SetActive(true);
+            interacting = true;
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            EndInteraction();
+            Popup.SetActive(false);
+            interacting = false;
         }
     }
 
     virtual public void StartInteraction()
     {
-        Popup.SetActive(true);
+        Debug.Log("Start");
 
     }
 
     virtual public void EndInteraction()
     {
-        Popup.SetActive(false);
+        Debug.Log("End");
     }
-
 
 }
