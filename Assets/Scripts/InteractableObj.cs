2fd84ad79e7deb11ad2a3a7400085957ddcc33cf
MitosisMan <99696985+MitosisMan@users.noreply.github.com>
2025-02-05 18:15:08 -0600

    Multiple Interactables

--- Assets/Scripts/InteractableObj.cs
+++ Assets/Scripts/InteractableObj.cs
@@ -5,37 +5,37 @@ using UnityEngine;
 public class InteractableObj : MonoBehaviour
 {
     [SerializeField] private GameObject Popup;
+    public bool active = false;
+    [SerializeField] private PlayerInteractor plint;
+    public float frame = 0;
 
     private void Start()
     {
         Popup.SetActive(false);
+        plint = GameObject.FindGameObjectsWithTag("Player")[0].GetComponent<PlayerInteractor>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") && collision.gameObject.GetComponent<PlayerInteractor>().enabled)
         {
-            StartInteraction();
+            plint.list.Add(this);
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            EndInteraction();
+            plint.list.Remove(this);
+            active = false;
         }
     }
 
-    virtual public void StartInteraction()
-    {
-        Popup.SetActive(true);
-
+    void Update(){
+        if(active){
+            Popup.SetActive(true);
+        }else{
+            Popup.SetActive(false);
+        }
     }
-
-    virtual public void EndInteraction()
-    {
-        Popup.SetActive(false);
-    }
-
-
 }
