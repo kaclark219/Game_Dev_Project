6657d905a3988db0f47a53034cefe90108ca2a8d
MitosisMan <99696985+MitosisMan@users.noreply.github.com>
2025-02-18 17:20:50 -0600

    Player Animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,10 +7,12 @@ public class PlayerMovement : MonoBehaviour
     private float speed = 4.0f;
     private Rigidbody2D rb;
     [SerializeField] private Vector2 dir;
+    Animator anim;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
@@ -19,5 +21,10 @@ public class PlayerMovement : MonoBehaviour
         dir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         dir = Vector3.Normalize(dir);
         rb.velocity = dir * speed;
+        if(dir.x != 0 || dir.y != 0){
+            anim.SetFloat("X", dir.x);
+            anim.SetFloat("Y", dir.y);
+        }
+        anim.SetBool("IsMoving", dir != Vector2.zero);
     }
 }
