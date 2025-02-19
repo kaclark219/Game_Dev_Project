d38b33f90de966de6e31df27559aabf16dc976ac
smsmeltzer <ssmeltzer@tamu.edu>
2025-02-19 15:11:22 -0600

    Added Day Night Cycle Lighting

---
+++ Assets/Scripts/DayNightCycle.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class DayNightCycle : MonoBehaviour
+{
+    public Light2D DayLight;
+    public Gradient DayLightGradient;
+
+    public Light2D NightLight;
+    public Gradient NightLightGradient;
+
+    public void UpdateLight(int energy)
+    {
+        float ratio = (100 - energy) / 100.0f;
+        DayLight.color = DayLightGradient.Evaluate(ratio);
+        NightLight.color = NightLightGradient.Evaluate(ratio);
+
+        transform.rotation = Quaternion.Euler(0, 0, 360.0f * ratio);
+    }
+}
+
